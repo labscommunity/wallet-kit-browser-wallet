dde4c57b6723ced7b148bdef6d9375884f6d4bfd
Marton Lederer <marton@lederer.hu>
2023-07-14 18:19:10 +0200

    fix: call window api from the core module

--- src/BrowserWalletStrategy.ts
+++ src/BrowserWalletStrategy.ts
@@ -1,4 +1,4 @@
-import { AppInfo, DispatchResult, GatewayConfig, PermissionType } from "@arweave-wallet-kit/core/wallet";
+import { AppInfo, callWindowApi, DispatchResult, GatewayConfig, PermissionType } from "@arweave-wallet-kit/core/wallet";
 import type { SignatureOptions } from "arweave/node/lib/crypto/crypto-interface";
 import type { Strategy } from "@arweave-wallet-kit/core/strategy";
 import type Transaction from "arweave/node/lib/transaction";
@@ -55,34 +55,34 @@ export default class BrowserWalletStrategy implements Strategy {
     appInfo?: AppInfo,
     gateway?: GatewayConfig
   ): Promise<void> {
-    return await this.callWindowApi("connect", [permissions, appInfo, gateway]);
+    return await callWindowApi("connect", [permissions, appInfo, gateway]);
   }
 
   public async disconnect(): Promise<void> {
-    return await this.callWindowApi("disconnect");
+    return await callWindowApi("disconnect");
   }
 
   public async getActiveAddress(): Promise<string> {
-    return await this.callWindowApi("getActiveAddress");
+    return await callWindowApi("getActiveAddress");
   }
 
   public async getAllAddresses(): Promise<string[]> {
-    return await this.callWindowApi("getAllAddresses");
+    return await callWindowApi("getAllAddresses");
   }
 
   public async getPermissions(): Promise<PermissionType[]> {
-    return await this.callWindowApi("getPermissions");
+    return await callWindowApi("getPermissions");
   }
 
   public async getWalletNames(): Promise<{ [addr: string]: string }> {
-    return await this.callWindowApi("getWalletNames");
+    return await callWindowApi("getWalletNames");
   }
 
   public async sign(
     transaction: Transaction,
     options?: SignatureOptions
   ): Promise<void> {
-    const signedTransaction = await this.callWindowApi("sign", [
+    const signedTransaction = await callWindowApi("sign", [
       transaction,
       options
     ]);
@@ -100,33 +100,33 @@ export default class BrowserWalletStrategy implements Strategy {
     data: BufferSource,
     algorithm: RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams
   ): Promise<Uint8Array> {
-    return await this.callWindowApi("encrypt", [data, algorithm]);
+    return await callWindowApi("encrypt", [data, algorithm]);
   }
 
   public async decrypt(
     data: BufferSource,
     algorithm: RsaOaepParams | AesCtrParams | AesCbcParams | AesGcmParams
   ): Promise<Uint8Array> {
-    return await this.callWindowApi("decrypt", [data, algorithm]);
+    return await callWindowApi("decrypt", [data, algorithm]);
   }
 
   public async getArweaveConfig(): Promise<GatewayConfig> {
-    return await this.callWindowApi("getArweaveConfig");
+    return await callWindowApi("getArweaveConfig");
   }
 
   public async signature(
     data: Uint8Array,
     algorithm: AlgorithmIdentifier | RsaPssParams | EcdsaParams
   ): Promise<Uint8Array> {
-    return await this.callWindowApi("signature", [data, algorithm]);
+    return await callWindowApi("signature", [data, algorithm]);
   }
 
   public async getActivePublicKey(): Promise<string> {
-    return await this.callWindowApi("getActivePublicKey");
+    return await callWindowApi("getActivePublicKey");
   }
 
   public async dispatch(transaction: Transaction): Promise<DispatchResult> {
-    return await this.callWindowApi("dispatch", [transaction]);
+    return await callWindowApi("dispatch", [transaction]);
   }
 
   public addAddressEvent(listener: (address: string) => void) {
@@ -141,33 +141,5 @@ export default class BrowserWalletStrategy implements Strategy {
     listener: (e: CustomEvent<{ address: string }>) => void
   ) {
     removeEventListener("walletSwitch", listener);
-  }
-
-  /**
-   * Call the window.arweaveWallet API and wait for it to be injected,
-   * if it has not yet been injected.
-   *
-   * @param fn Function name
-   * @param params Params
-   * @returns API result
-   */
-  async callWindowApi(fn: string, params: any[] = []) {
-    // if it is already injected
-    if (window?.arweaveWallet) {
-      // @ts-expect-error
-      return await window.arweaveWallet[fn as any](...params);
-    }
-
-    // if it has not yet been injected
-    return new Promise((resolve, reject) =>
-      window.addEventListener("arweaveWalletLoaded", async () => {
-        try {
-          // @ts-expect-error
-          resolve(await window.arweaveWallet[fn as any](...params));
-        } catch (e) {
-          reject(e);
-        }
-      })
-    );
   }
 }
